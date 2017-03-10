d6ad8aaad72704d3a564ca03e84f8df774cf6b72
Lei <zhenglei@ourgame.com>
2017-03-10 18:46:56 +0800

    Updated to batch request for Facebook notifications.

--- app/tasks/promotion.py
+++ app/tasks/promotion.py
@@ -1,5 +1,7 @@
 import grequests
 import hashlib
+import json
+import urllib.parse
 
 from functools import partial
 
@@ -37,11 +39,28 @@ def process_facebook_notification(push_id=None):
     pushes = db.session.query(PromotionPush).filter(PromotionPush.id.in_(list(set(push_ids)))).all()
     id_to_message_mapping = {item.id: item.message for item in pushes}
 
+    # reqs = []
+    # for item in push_histories:
+    #     data = {'access_token': '122212108221118|iY2aFJsHhyWUvTV_4oeoGrXX-TA', 'template': id_to_message_mapping[item.push_id]}
+    #     headers = {'X-Correlation-Id': str(item.id)}
+    #     req = grequests.post('https://graph.facebook.com/v2.7/' + item.target + '/notifications',
+    #                          data=data,
+    #                          headers=headers,
+    #                          timeout=5
+    #                         )
+    #     reqs.append(req)
+
     reqs = []
-    for item in push_histories:
-        data = {'access_token': '122212108221118|iY2aFJsHhyWUvTV_4oeoGrXX-TA', 'template': id_to_message_mapping[item.push_id]}
-        headers = {'X-Correlation-Id': str(item.id)}
-        req = grequests.post('https://graph.afacebook.com/v2.7/' + item.target + '/notifications',
+    iter_step = 50
+    for i in range(0, len(push_histories), iter_step):
+        group = push_histories[i:i+iter_step]
+
+        data = {'access_token': '122212108221118|iY2aFJsHhyWUvTV_4oeoGrXX-TA',
+                'include_headers': 'false',
+                'batch': json.dumps([{'method': 'POST', 'relative_url': 'v2.7/' + item.target + '/notifications', 'body' : urllib.parse.urlencode({'template': id_to_message_mapping[item.push_id]})} for item in group])}
+        headers = {'X-Correlation-Ids': ','.join([item.id for item in group])}
+
+        req = grequests.post('https://graph.facebook.com',
                              data=data,
                              headers=headers,
                              timeout=5
@@ -54,21 +73,24 @@ def process_facebook_notification(push_id=None):
     print('process_facebook_notification: start sending')
 
     successed_push_history_ids = []
-    for resp in grequests.imap(reqs, size=50):
+    for resp in grequests.imap(reqs, size=10):
         if resp:
-            push_history_id = resp.request.headers['X-Correlation-Id']
-            push_history = db.session.query(PromotionPushHistory).filter_by(id=push_history_id).one()
+            push_history_ids = resp.request.headers['X-Correlation-Ids'].split(',')
 
-            content = resp.json()
-            if content['error']:
-                push_history.status = PROMOTION_PUSH_HISTORY_STATUSES.FAILED.value
-                push_history.error_message = content['error']['message']
-            else:
-                push_history.status = PROMOTION_PUSH_HISTORY_STATUSES.SUCCESS.value
-                push_history.error_message = None
-            db.session.commit()
+            responses = resp.json()
+            for idx, response in enumerate(responses):
+                push_history_id = push_history_ids[idx]
+                push_history = db.session.query(PromotionPushHistory).filter_by(id=push_history_id).one()
+                err = response['body']['error']
+                if err:
+                    push_history.status = PROMOTION_PUSH_HISTORY_STATUSES.FAILED.value
+                    push_history.error_message = err['message']
+                else:
+                    push_history.status = PROMOTION_PUSH_HISTORY_STATUSES.SUCCESS.value
+                    push_history.error_message = None
+                db.session.commit()
 
-            successed_push_history_ids.append(push_history_id)
+                successed_push_history_ids.append(push_history_id)
 
     print('process_facebook_notification: finish sending')
 
