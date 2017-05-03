ddaa41f7f6df7fd39a660a263e6a17594ced1971
樊海鹏 <fanhaipeng0403@126.com>
2017-05-03 15:42:08 +0800

    add different platform revenue

--- app/tasks/bi_statistic/revenue.py
+++ app/tasks/bi_statistic/revenue.py
@@ -60,3 +60,55 @@ def process_bi_statistic_revenue(target):
                 print(target + ' revenue transaction.commit()')
 
         with_db_context(db, sync_collection_revenue)
+
+    def collection_revenue_every_platform(connection, transaction):
+
+        if target == 'lifetime':
+
+            return connection.execute(text("""
+                                           SELECT DATE(CONVERT_TZ(created_at, '+00:00', :timezone_offset)) AS on_day,
+                                                  platform,
+                                                  ROUND(SUM(currency_amount), 2)                           AS sum
+                                           FROM   bi_user_bill
+                                           WHERE  currency_type = 'Dollar'
+                                           GROUP  BY on_day,platform
+                                            """), timezone_offset=timezone_offset)
+        else:
+
+            return connection.execute(text("""
+                                           SELECT ROUND(SUM(currency_amount), 2)                           AS sum,
+                                                  platform
+                                           FROM   bi_user_bill
+                                           WHERE  currency_type = 'Dollar'
+                                           AND  DATE(CONVERT_TZ(created_at, '+00:00', :timezone_offset))  = :on_day
+                                           GROUP BY platform
+                                           """), timezone_offset=timezone_offset, on_day=someday)
+
+    result_proxy = with_db_context(db, collection_revenue_every_platform)
+
+    if target == 'lifetime':
+
+        rows = [{'_on_day': row['on_day'], '_platform': row['platform'], 'sum': row['sum']} for row in result_proxy]
+    else:
+
+        rows = [{'_on_day': someday, '_platform': row['platform'], 'sum': row['sum']} for row in result_proxy]
+    if rows:
+
+        def sync_collection_revenue_every_platform(connection, transaction):
+
+            where = and_(BIStatistic.__table__.c.on_day == bindparam('_on_day'),
+                         BIStatistic.__table__.c.game == 'All Game',
+                         BIStatistic.__table__.c.platform == bindparam('_platform'))
+            values = {'revenue': bindparam('sum')}
+
+            try:
+                connection.execute(BIStatistic.__table__.update().where(where).values(values), rows)
+            except:
+                print(target + ' revenue for every platform transaction.rollback()')
+                transaction.rollback()
+                raise
+            else:
+                transaction.commit()
+                print(target + ' revenue for every platform transaction.commit()')
+
+        with_db_context(db, sync_collection_revenue_every_platform)
