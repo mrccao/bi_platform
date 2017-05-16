1fefb44da571214a3d621b8e1fd358a008fee6fd
樊海鹏 <fanhaipeng0403@126.com>
2017-05-16 16:55:25 +0800

    update user region lifetime

--- app/controllers/report/__init__.py
+++ app/controllers/report/__init__.py
@@ -4,4 +4,4 @@ report = Blueprint('report', __name__)
 
 from .daily_summary import daily_summary
 from .new_users import new_users
-from .user_map import user_map
+from .user_map import user_region
--- app/controllers/report/user_map.py
+++ app/controllers/report/user_map.py
@@ -1,4 +1,3 @@
-import copy
 from itertools import groupby
 
 from flask import current_app as app, jsonify
@@ -12,10 +11,10 @@ from app.utils import current_time
 from . import report
 
 
-@report.route("/report/user_map", methods=["GET"])
+@report.route("/report/user_region", methods=["GET"])
 # @login_required
-def user_map():
-    return render_template('report/users_map.html')
+def user_region():
+    return render_template('report/user_region.html')
 
 
 @report.route("/report/reg_user_state", methods=["GET"])
@@ -35,7 +34,7 @@ def get_reg_user_state_data():
                                                 ORDER BY value DESC  LIMIT 10
                                                   """))
 
-        query_result =list(query_result)
+        query_result = list(query_result)
         transpose_query_result = list(map(list, zip(*query_result)))
 
         query_result_row_dict = []
@@ -49,7 +48,7 @@ def get_reg_user_state_data():
         bar_result = {'location': location, 'reg_count': reg_count}
         map_result = query_result_row_dict
 
-        return jsonify(map_result=map_result, bar_result=bar_result,time_range='lifetime')
+        return jsonify(map_result=map_result, bar_result=bar_result, type='lifetime')
 
 
     elif group == 'Monthly':
@@ -86,9 +85,9 @@ def get_reg_user_state_data():
                                                   """), timezone_offset=timezone_offset, start_time=start_time,
                                          end_time=end_time)
 
-    query_result =list(query_result)
+    query_result = list(query_result)
     transpose_query_result = list(map(list, zip(*query_result)))
-    time_range =sorted(list(set(transpose_query_result[0])))
+    time_range = sorted(list(set(transpose_query_result[0])))
 
     # bar
 
@@ -100,14 +99,12 @@ def get_reg_user_state_data():
         query_result_row_dict.append(row_dict)
 
     query_result_row_dict.sort(key=itemgetter('time'))
-    reg_user_state_group_by_time =groupby(query_result_row_dict, itemgetter('time'))
-
+    reg_user_state_group_by_time = groupby(query_result_row_dict, itemgetter('time'))
 
     for time, group in reg_user_state_group_by_time:
 
-
-        group=list(group)
-        group.sort(key=itemgetter('value'),reverse=True)
+        group = list(group)
+        group.sort(key=itemgetter('value'), reverse=True)
         ten_top_reg_countries = group[:11]
         location = []
         reg_count = []
@@ -115,6 +112,7 @@ def get_reg_user_state_data():
         for row in ten_top_reg_countries:
             location.append(row['name'])
             reg_count.append(row['value'])
+
         bar_result.append({time: {'location': location, 'reg_count': reg_count}})
 
     # map
@@ -130,109 +128,3 @@ def get_reg_user_state_data():
         map_result.append({time: group})
 
     return jsonify(time_range=time_range, bar_result=bar_result, map_result=map_result)
-
-
-#
-# @report.route("/report/reg_user_country", methods=["GET"])
-# # @login_required
-# def get_reg_user_country_data():
-#     group = request.args.get('Group')
-#     now = current_time(app.config['APP_TIMEZONE'])
-#     timezone_offset = app.config['APP_TIMEZONE']
-#
-#     if group == 'lifetime':
-#
-#         query_result = db.engine.execute(text("""
-#                                                 SELECT    reg_country  AS name,  count(user_id)                                     AS value
-#                                                 FROM      bi_user
-#                                                 GROUP BY  reg_country
-#                                                 ORDER BY value DESC
-#                                                   """))
-#
-#         reg_user_country = []
-#         for row in query_result:
-#             row_dict = dict(row)
-#             reg_user_country.append(row_dict)
-#
-#         # ten_top_reg_countries
-#         bar_result = reg_user_country[:11]
-#
-#         return jsonify(reg_user_country=reg_user_country, bar_result=bar_result)
-#
-#
-#
-#     elif group == 'Monthly':
-#
-#         start_time = now.replace(days=-int(180)).format('YYYY-MM-DD')
-#         end_time = now.replace(days=-1).format('YYYY-MM-DD')
-#
-#         query_result = db.engine.execute(text("""
-#                                                 SELECT    DATE_FORMAT(CONVERT_TZ(reg_time, '+00:00', :timezone_offset),'%Y-%m') AS time,
-#                                                           reg_country  AS name,  count(user_id)                                     AS value
-#                                                 FROM      bi_user
-#                                                 WHERE     DATE(CONVERT_TZ(reg_time, '+00:00', :timezone_offset))
-#                                                             BETWEEN :start_time AND :end_time
-#                                                 GROUP  BY time,reg_country
-#                                                   """), timezone_offset=timezone_offset, start_time=start_time,
-#                                          end_time=end_time)
-#
-#     else:
-#
-#         start_time = now.replace(days=-int(30)).format('YYYY-MM-DD')
-#         end_time = now.replace(days=-1).format('YYYY-MM-DD')
-#
-#         query_result = db.engine.execute(text("""
-#                                                 SELECT DATE(CONVERT_TZ(reg_time, '+00:00', :timezone_offset)) AS time,
-#                                                        reg_country                                                AS name,
-#                                                        Count(*)                                               AS value
-#                                                 FROM   bi_user
-#                                                 WHERE  DATE(CONVERT_TZ(reg_time, '+00:00', :timezone_offset)) BETWEEN
-#                                                        :start_time AND :end_time
-#                                                 GROUP  BY time,
-#                                                           reg_country
-#                                                   """), timezone_offset=timezone_offset, start_time=start_time,
-#                                          end_time=end_time)
-#
-#     # time_range
-#
-#     transpose_query_result = list(map(list, zip(*query_result)))
-#     time_range = list(set(transpose_query_result[0]))
-#     time_range = time_range.sort(key=time_range.index)
-#
-#     # bar
-#
-#     bar_result = []
-#     value = []
-#     reg_user_country = []
-#
-#     for row in query_result:
-#         row_dict = dict(row)
-#         reg_user_country.append(row_dict)
-#
-#     reg_user_country.sort(key=itemgetter('time'))
-#     reg_user_country_group_by_time = groupby(reg_user_country, itemgetter('time'))
-#
-#     for time, group in reg_user_country_group_by_time:
-#
-#         group.sort(key=itemgetter('value'))
-#         ten_top_reg_countries = group[:11]
-#
-#         for country in ten_top_reg_countries:
-#             country.append(country['country'])
-#             value.append(country['value'])
-#             bar_result.append({time: {'country': country, 'value': value}})
-#
-#     # map
-#
-#     reg_user_country = []
-#     for row in query_result:
-#         row_dict = dict(row)
-#         del row_dict['time']
-#         reg_user_country.append(row_dict)
-#
-#     reg_user_country.sort(key=itemgetter('time'))
-#     reg_user_country_group_by_time = groupby(reg_user_country, itemgetter('time'))
-#
-#     map_result = reg_user_country_group_by_time
-#
-#     return jsonify(time_range=time_range, bar_result=bar_result, map_result=map_result)
